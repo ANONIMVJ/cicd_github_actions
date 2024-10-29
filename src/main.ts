f608862cf7078bdd21d03f2fa62c62d9e715b379
JavohirViktorov <sypheranonimus@gmail.com>
2024-10-30 01:02:34 +0500

    Added deploy to VPS

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function start() {
   const PORT = process.env.PORT;
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
   });
 }
-bootstrap();
+start();
 
 
